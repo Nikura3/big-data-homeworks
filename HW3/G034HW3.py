7707fcdd6c63dea17d09623854dcc184f0ce0f24
Nicla <faccioli.nicla@gmail.com>
2024-06-14 11:02:50 +0200

    fix: variables names + rounding r for sticky sampling

--- HW3/G034HW3.py
+++ HW3/G034HW3.py
@@ -7,12 +7,12 @@ import sys
 import random
 import math
 
-def exactCount(rdd, candidates, global_count, stopping_condition, n):
+def exactCount(rdd, r_candidates, r_global_count, stopping_condition, n):
     items = rdd.collect()
     for item in items:
-        candidates[item] += 1
-        global_count[0] += 1
-        if global_count[0] >= n:
+        r_candidates[item] += 1
+        r_global_count[0] += 1
+        if r_global_count[0] >= n:
             stopping_condition.set()
             return
 
@@ -23,20 +23,20 @@ def reservoirSampling(rdd, reservoir, m, t, n):
         if len(reservoir) < m:
             reservoir.append(item)
         else:
-            s = random.randint(0, t[0] - 1)
-            if s < m:
-                reservoir[s] = item
+            if random.random() <= m / t[0]:
+                el = random.randint(0, len(reservoir) - 1)
+                reservoir[el] = item
         if t[0] >= n:
             return
 
-def stickySampling(rdd, hash_table, hash_table_size, item_count, n):
+def stickySampling(rdd, hash_table, r, item_count, n):
     items = rdd.collect()
     for item in items:
         item_count[0] += 1
         if item in hash_table:
             hash_table[item] += 1
         else:
-            if random.random() <= hash_table_size / n:
+            if random.random() <= r / n:
                 hash_table[item] = 1
         if item_count[0] >= n:
             return
@@ -61,20 +61,20 @@ if __name__ == '__main__':
     print("INPUT PROPERTIES")
     print(f"n = {n} phi = {phi} epsilon = {epsilon} delta = {delta} port = {portExp}")
 
-    candidates = defaultdict(int)
-    global_count = [0]
+    r_candidates = defaultdict(int)
+    r_global_count = [0]
     t = [0]
     m = math.ceil(1 / phi)
     reservoir = []
     item_count = [0]
-    hash_table_size = (1 / epsilon) * math.log(1 / (delta * phi))
+    r = math.ceil((1 / epsilon) * math.log(1 / (delta * phi)))
     hash_table = defaultdict(int)
 
     stream = ssc.socketTextStream("algo.dei.unipd.it", portExp, StorageLevel.MEMORY_AND_DISK)
 
-    stream.foreachRDD(lambda time, rdd: exactCount(rdd, candidates, global_count, stopping_condition, n))
+    stream.foreachRDD(lambda time, rdd: exactCount(rdd, r_candidates, r_global_count, stopping_condition, n))
     stream.foreachRDD(lambda time, rdd: reservoirSampling(rdd, reservoir, m, t, n))
-    stream.foreachRDD(lambda time, rdd: stickySampling(rdd, hash_table, hash_table_size, item_count, n))
+    stream.foreachRDD(lambda time, rdd: stickySampling(rdd, hash_table, r, item_count, n))
     
     def stop_streaming(ssc, stopping_condition):
         stopping_condition.wait()
@@ -86,13 +86,13 @@ if __name__ == '__main__':
     ssc.start()
     stop_thread.join()
 
-    true_frequent_items = {item for item, count in candidates.items() if count >= phi * n}
+    true_frequent_items = {item for item, count in r_candidates.items() if count >= phi * n}
     estimated_frequent_items_reservoir = {item for item in reservoir}
-    estimated_frequent_items_sticky = {item: count for item, count in hash_table.items() if count >= (phi - epsilon) * n}
+    estimated_frequent_items_sticky = {item for item, count in hash_table.items() if count >= (phi - epsilon) * n}
 
     # Output for EXACT ALGORITHM
     print("EXACT ALGORITHM")
-    print(f"Number of items in the data structure = {len(candidates)}")
+    print(f"Number of items in the data structure = {len(r_candidates)}")
     print(f"Number of true frequent items = {len(true_frequent_items)}")
     print("True frequent items:")
     for item in sorted(true_frequent_items, key=lambda x: int(x)):
@@ -112,6 +112,6 @@ if __name__ == '__main__':
     print(f"Number of items in the Hash Table = {len(hash_table)}")
     print(f"Number of estimated frequent items = {len(estimated_frequent_items_sticky)}")
     print("Estimated frequent items:")
-    for item in sorted(estimated_frequent_items_sticky):
+    for item in sorted(estimated_frequent_items_sticky, key=lambda x: int(x)):
         sign = "+" if item in true_frequent_items else "-"
         print(f"{item} {sign}")
